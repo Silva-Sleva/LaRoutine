6bbb78e7f62ff75fd05f49af83a87a3c6c742fcf
j4kov <j4kovlev.vyach@yandex.ru>
2025-04-27 16:07:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, url_for, render_template, redirect
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+def start():
+    return render_template('base.html', title='Миссия Колонизация Марса')
+
+
+@app.route('/bags')
+def bags():
+    return render_template('bags.html', title='Миссия Колонизация Марса')
+
+@app.route('/tshirt')
+def tshirt():
+    return render_template('tshirt.html', title='Миссия Колонизация Марса')
+
+
+def main():
+    # db_session.global_init("db/blogs.db")
+    # add_user()
+    # add_job()
+    app.run(port=8081, host='127.0.0.1', debug=True)
+
+if __name__ == '__main__':
+    main()
